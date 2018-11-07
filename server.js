91204193f8d8b21ffe05f94e70af40f74705655c
Catalina Orellana <catalina.orellana@2brains.cl>
2018-11-06 21:43:21 -0300

    Conecta a base de datos

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
 // Declare dependencies
 const express = require('express');
 const app = express();
-// app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }));
-// app.use(bodyParser.json({ limit: '5mb' }));
-// // app.use(expressPrettify({ query: 'pretty' }));
-// // app.use(compression({ filter: shouldCompress }));
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   next();
-// });
+const mongoose = require('mongoose');
 
-// app.use(passport.initialize());
-
-// Setting routes and populate
-// require('./api/api')(app);
-
-// Setting static pages
-// app.use(express.static(`${__dirname}/public`));
-// require('./public/index.routes').default(app);
+// Conectar a base de datos
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/chef-now';
+mongoose.connect(mongoUri);
+mongoose.connection.on('error', (error) => {
+  console.log('Error conectandose a bbdd', error);
+  process.exit(-1);
+});
 
 // wake up
 app.get('/test', (req, res) => {
-  res.send('Bienvenido postulante!')
+  res.send('hola...');
 });
 
 // Run server
