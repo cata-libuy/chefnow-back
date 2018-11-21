9f20c053ed2b01d8d54a5b520e192410db60b0d4
Catalina Orellana <catalina.orellana@2brains.cl>
2018-11-20 21:24:13 -0300

    Agrega imagen a receta

--- api/receta/receta.model.js
+++ api/receta/receta.model.js
@@ -3,27 +3,30 @@ const mongoose = require('mongoose');
 
 // Setting model
 const RecetaSchema = new mongoose.Schema({
-titulo: {
-  type: String,
-  required: true,
+  titulo: {
+    type: String,
+    required: true,
+    },
+  cuerpo: {
+    type: String,
+    required: true,   
   },
-cuerpo: {
-  type: String,
-  required: true,   
-},
-tiempoPreparacion: {
-  type: Number,
-  required: true,   
-},  
-porcion: {
-  type: Number,
-  required: true,   
-},
-deleted:{
-  type: Boolean,
-  default: false
-}  
-  
+  tiempoPreparacion: {
+    type: Number,
+    required: true,   
+  },  
+  porcion: {
+    type: Number,
+    required: true,   
+  },
+  deleted:{
+    type: Boolean,
+    default: false
+  },
+  imagenPrincipal: {
+    type: String,
+    default: 'https://e-fisiomedic.com/wp-content/uploads/2013/11/default-placeholder-300x300.png'
+  }  
 });
 
 // Export model for endpoint
